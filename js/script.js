9ba5b84ae2426977f2d0877980d10955fff1e319
Zachary Gichiri <gichirigichuru@gmail.com>
2022-01-13 10:01:36 +0300

    hidding and viewing design details done

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#icon1").click(function(){
+        $(this).hide()
+        $("#what1").show()
+    })
+    $("#what1").click(function(){
+        $(this).hide()
+        $("#icon1").show()
+    })
+
+
+
+
+})
