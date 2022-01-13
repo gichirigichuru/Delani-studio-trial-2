cf908dea523cbe012500b7e6ddc5a7a067044bb1
Zachary Gichiri <gichirigichuru@gmail.com>
2022-01-13 10:09:29 +0300

    development section hide and show when clicked

--- js/script.js
+++ js/script.js
@@ -7,7 +7,22 @@ $(document).ready(function(){
         $(this).hide()
         $("#icon1").show()
     })
-
+    $("#icon2").click(function(){
+        $(this).hide()
+        $("#what2").show()
+    })
+    $("#what2").click(function(){
+        $(this).hide()
+        $("#icon2").show()
+    })
+    $("#icon3").click(function(){
+        $(this).hide()
+        $("#what3").show()
+    })
+    $("#what3").click(function(){
+        $(this).hide()
+        $("#i").show()
+    })
 
 
 
